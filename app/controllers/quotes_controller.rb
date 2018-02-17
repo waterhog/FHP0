fdb3876ef70f1bad9e2e6954efad690b405a36d0
Geoffrey Waterhouse <geoff.s.waterhouse@gmail.com>
2018-02-17 05:33:54 +0000

    Can submit new quotes.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,4 +8,13 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+
 end
